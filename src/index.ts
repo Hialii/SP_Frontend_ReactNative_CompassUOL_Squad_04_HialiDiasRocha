f4d055459bb7dc6adf103afeb057b277d6ce1a8e
Hiali <hialidias@gmail.com>
2023-08-21 14:46:57 -0300

    last commit

--- src/index.ts
+++ src/index.ts
@@ -18,24 +18,27 @@
     .catch(error => console.error('ERROR', error.mesage))
 
   
-
- function createNewArticle(items: Mock[]) {
+//============POSTS================
+ function createNewArticle(items: Mock[]): any {
    const HTMLElement = document.getElementById('article');
 
    if(HTMLElement) {
       const article = document.createElement('article');
-      article.className = 'principal';
+      article.className = 'principal flexbox';
 
       items.map(art => {
          const articleLink =  document.createElement('a');
+         articleLink.className = ('posts')
          articleLink.href = `${art.url}`;
 
-         articleLink.innerHTML = 
-         `<img class="article-img" src="${art.image}" alt="${art.alt}">
+         articleLink.innerHTML =
+         `<div class="">
+         <img class="article-img" src="${art.image}" alt="${art.alt}">
          <div class="article-text text">
             <h3>${art.title}</h3>
             <p>${art.text}</p>
             <a href=""></a>
+         </div>
          </div>`
        article.appendChild(articleLink);
       })
@@ -50,6 +53,7 @@
 interface Content {
    id: number;
    image: string;
+   image2: string;
    alt: string;
    title: string;
    subtitle: string;
@@ -74,7 +78,8 @@ if (getId) {
          searchItem(parseInt(getId), data.matter);
       })
       .catch(error => console.error('ERROR', error.message));
-   fetch('../api/coments.json')
+   
+      fetch('../api/coments.json')
       .then(res => res.json())
       .then((data: { coments: Coments[] }) => {
          searchComent(parseInt(getId), data.coments);
@@ -87,19 +92,21 @@ if (getId) {
 
 
 //==================MATTER=========================
-function searchItem(id: number, pageContent: Content[]) {
+function searchItem(id: number, pageContent: Content[]): any {
    const findItem = pageContent.find(cont => cont.id === id);
    
    if(findItem){
-      const section = document.getElementById('aticle-content');
+      const section = document.getElementById('article-content');
       section!.innerHTML = 
-      `<img class="principal-img"" src="${findItem.image}" alt="${findItem.alt}"> 
-      <div class="principal-text text">
-      <h1>${findItem.title}</h1>
-      <p>${findItem.subtitle}</p>
-      </div>
-      <div class="divider"></div>
-      <p class="container">${findItem.text}</p>`
+      `
+      <h1 class="principal-text text">${findItem.title}</h1>
+     <img class="principal-img center" src="${findItem.image}" alt="${findItem.alt}">   
+      <p class="principal-text text">${findItem.subtitle}</p> 
+      <div class="divider"></div>  
+      <p class="container p">${findItem.text}</p>
+      <img class="principal-img center" src="${findItem.image2}" alt="${findItem.alt}">
+      <p class="container p">${findItem.text}</p> 
+      </div>`
    }
 }
 //====================COMENTS============================
@@ -110,17 +117,29 @@ function searchComent(id: number, coment: Coments[]) {
    if(findItem) {
       const coments = document.getElementById('coment');
       coments!.innerHTML =
-      `<div class="coment-header">
-         <img class="coment-img" src="${findItem.img}" alt="${findItem.alt}">
-         <div>
-         <h4 class="coment-name">${findItem.name}</h4>
-         <p class="coment-data">${findItem.data}</p>
+      `<div class="p">
+      <h3 class="author-coment">Author Coment</h3>
+      <div class="divider"></div>
+      <div class="coment-header">
+         <div class="coment-header">
+            <img class="coment-img" src="${findItem.img}" alt="${findItem.alt}">
+            <div>
+               <h4 class="coment-name">${findItem.name}</h4>
+               <p class="coment-data">${findItem.data}</p>
+            </div>
          </div>
-       </div>
-       <p class="coment-text">${findItem.text}</p>`
+         <div>
+            <a href="https://pt-br.facebook.com/" target="_blank"><img class="social-img" src="../assets/5279111_network_fb_social media_facebook_facebook logo_icon.svg" alt="facebook logo"></a>
+            <a href="https://medium.com/" target="_blank"><img class="social-img" src="../assets/7088889_medium_medium logo_icon.svg" alt="$medium logo"></a>
+         </div>
+      </div>  
+         <p class="coment-text">${findItem.text}</p>
+       
+      `
    }
    
 }
+
 
 
 
